2c2c267580fbebda7d9b1f3b3d2d9a96df3cce8b
Vincent Nabet <vnabet@groupeisagri.com>
2023-09-06 17:37:22 +0200

    Modification de l'intercepteur sur les requêtes en cours

--- src/app/core/services/httpState.service.ts
+++ src/app/core/services/httpState.service.ts
@@ -3,25 +3,42 @@ import { Injectable } from '@angular/core';
 
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
+/**
+ * Service d'état des requêtes HTTP
+ * Renseigné grâce à l'intercepteur LoadingInterceptorService
+ */
 @Injectable()
 export class HttpStateService {
 
   private _loading:BehaviorSubject<boolean> = new BehaviorSubject(false);
   private _error:Subject<string> = new Subject();
+  // liste de requêtes en cours
+  // Rq: un compteur ne marche pas, car une requête peut être annulée
+  private _requests:string[] = [];
 
+  // A true si on a au moins une requête en cours
   public loading:Observable<boolean> = this._loading.asObservable();
+  // Remonte les éventuelles erreurs
   public error:Observable<string> = this._error.asObservable();
 
   constructor() { }
 
-  load() {
+  // On incrémente une requête
+  load(request:string) {
+    if(this._requests.indexOf(request) === -1) this._requests.push(request);
     this._loading.next(true);
   }
 
-  complete() {
-    this._loading.next(false);
+  // On décrémente les requêtes
+  complete(request:string) {
+    const index:number = this._requests.indexOf(request);
+    if(index > -1) this._requests.splice(index, 1);
+    if(!this._requests.length) {
+      this._loading.next(false);
+    }
   }
 
+  // Remontée des erreurs
   catchError(err:HttpErrorResponse) {
     this._error.next(`${err.status} - ${err.message}`);
   }
--- src/app/core/services/loading-interceptor.service.ts
+++ src/app/core/services/loading-interceptor.service.ts
@@ -3,20 +3,29 @@ import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 import { Injectable } from '@angular/core';
 import { HttpStateService } from './httpState.service';
 
+/**
+ * Intercepteur de requête HTTP
+ * Permet de connaître l'état de chargement des requête via le service HttpStateService
+ */
 @Injectable()
 export class LoadingInterceptorService implements HttpInterceptor {
 
   constructor(private loading:HttpStateService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.loading.load();
+    //Quand une requête est en route
+    //On prévient le service HttpStateService qu'on est en chargement
+    this.loading.load(req.url);
     return next.handle(req).pipe(
       tap((event) => {
-        if(event instanceof HttpResponse) this.loading.complete();
+        // Fin de la requête
+        if(event instanceof HttpResponse) this.loading.complete(req.url);
       }),
       catchError((err:HttpErrorResponse) => {
+        // On remonte l'erreur
         this.loading.catchError(err);
-        this.loading.complete();
+        // Et fin de la requête
+        this.loading.complete(req.url);
         return throwError(()=> err);
       })
     )
