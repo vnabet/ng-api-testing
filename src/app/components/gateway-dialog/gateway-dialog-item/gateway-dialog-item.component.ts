a0dcf28b88a5b88e32d13381b7eea44809b4f3a7
Vincent Nabet <vnabet@groupeisagri.com>
2023-09-11 17:35:48 +0200

    ajout de commentaire de code

--- src/app/components/gateway-dialog/gateway-dialog-item/gateway-dialog-item.component.ts
+++ src/app/components/gateway-dialog/gateway-dialog-item/gateway-dialog-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
+/**
+ * Composant qui gère chaque ligne de la modale de gestion des Gateways
+ */
 @Component({
   selector: 'app-gateway-dialog-item',
   templateUrl: './gateway-dialog-item.component.html',
@@ -7,23 +10,34 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
 })
 export class GatewayDialogItemComponent {
 
+  // Flag pour savoir si est en mode édition
   editMode:boolean = false;
+  // On supprime une gateway une fois que l'on a cliqué 2 fois sur le bouton de suppression
+  // Ce flag permet de savoir que l'on a déjà cliqué une fois
   deleteClicked:boolean = false;
 
+  // Évènement de mise à jour d'une gateway
+  // On passe l'ancienne et le nouvelle valeur
   @Output()
   update:EventEmitter<{oldValue:string, newValue:string}> = new EventEmitter();
 
+  // Événement de suppression d'une gateway
   @Output()
   delete:EventEmitter<string> = new EventEmitter();
 
+  // Valeur de la gateway
   @Input()
   gateway:string = '';
 
+  // Doit-on afficher le bouton de suppression
+  // Non s'il n'y a plus qu'une seule gateway dans la liste de la modale
   @Input()
   displayDeleteBtn:boolean = true;
 
+  // Input de mise à jour de la gateway
+  // Il n'est pas toujours visible suivant le mode d'édition
+  // On passe donc par un setter/getter pour le récupérer
   private _input!:HTMLInputElement;
-
   @ViewChild('input')
   get input():HTMLInputElement {
     return this._input;
@@ -31,29 +45,46 @@ export class GatewayDialogItemComponent {
   set input(el:ElementRef) {
     if(el) {
       this._input = el.nativeElement as HTMLInputElement;
+      // On met le focus dessus dès qu'il s'affiche
       setTimeout(()=>el.nativeElement.focus());
     }
   }
 
+  /**
+   * Passage en mode édition
+   * Revient à afficher l'input de saisie
+   */
   editGateway() {
     this.editMode = true;
   }
 
+  /**
+   * Sauvegarde de la gateway
+   */
   saveGateway() {
+    // On repasse en mode lecture
     this.editMode = false;
+    // On annule l'éventuel premier click sur le bouton de suppression
     this.deleteClicked = false;
-9
+
     const newValue:string = this._input.value.trim();
 
+    // On s'assure que l'ancienne et la nouvelle valeur sont bien différentes
     if(newValue && newValue!==this.gateway) {
       const oldValue:string = this.gateway;
       this.gateway = newValue;
+      // On emet l'évènement de mise à jour
       this.update.emit({oldValue, newValue});
     }
   }
 
+  /**
+   * Suppression de la gateway
+   */
   deleteGateway() {
+    // Seulement au second click sur le bouton !
     if(this.deleteClicked) {
+      // On émet l'évènement de suppression
       this.delete.emit(this.gateway);
     } else {
       this.deleteClicked = true;
--- src/app/components/gateway-dialog/gateway-dialog.component.ts
+++ src/app/components/gateway-dialog/gateway-dialog.component.ts
@@ -1,6 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GatewaysService } from 'src/app/core';
 
+
+/**
+ * Fenêtre modale de gestion des Gateways
+ */
 @Component({
   selector: 'app-gateway-dialog',
   templateUrl: './gateway-dialog.component.html',
@@ -8,25 +12,39 @@ import { GatewaysService } from 'src/app/core';
 })
 export class GatewayDialogComponent {
 
+  // Champ d'ajout d'une Gateway
   @ViewChild('input') input!:ElementRef;
 
   constructor(public gateways:GatewaysService) {
 
   }
 
+  /**
+   * Mise à jour de la valeur d'une Gateway
+   * @param gateway ancienne et nouvelle valeur
+   */
   update(gateway:{oldValue:string, newValue:string}) {
     this.gateways.update(gateway.oldValue, gateway.newValue);
   }
 
+  /**
+   * Ajout d'une Gateway
+   * @param gateway Valeur de la Gateway
+   */
   add(gateway:string) {
     const gw = gateway.trim();
 
     if(gw) {
       this.gateways.add(gw);
+      // On vide l'input une fois la Gateway ajoutée
       (this.input.nativeElement as HTMLInputElement).value = '';
     }
   }
 
+  /**
+   * Suppression d'une Gateway
+   * @param gateway Valeur à supprimer
+   */
   delete(gateway:string) {
     if(gateway) {
       this.gateways.delete(gateway);
