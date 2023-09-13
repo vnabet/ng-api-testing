59606d0f3de0408ec1065b4e2b7fbba535ccfa55
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2023-09-13 17:14:14 +0200

    ajout des champs login et password

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, map, Subject, takeUntil } from 'rxjs';
 import { DomainsService } from 'src/app/authentication/services/domains.service';
 
@@ -12,18 +12,22 @@ export class LoginFormComponent implements OnInit, OnDestroy {
 
   private readonly destroy$ = new Subject();
 
+  private _validators = [
+    Validators.required
+  ]
 
-  clientId = new FormControl<string>(localStorage.getItem('clientId') ?? '');
 
-  domainId = new FormControl<number>(0);
+  clientId = new FormControl<string>(localStorage.getItem('clientId') ?? '', {validators: this._validators});
+
+  domainId = new FormControl<number>(0, {validators: this._validators});
 
   form = this.fb.group({
-    gateway: new FormControl<string>(''),
+    gateway: new FormControl<string>('', {validators: this._validators}),
     domainId: this.domainId,
     dataSetLabel: new FormControl<string>(''),
     clientId: this.clientId,
-    userLogin: new FormControl<string>(''),
-    userPassword: new FormControl<string>('')
+    userLogin: new FormControl<string>('', {validators: this._validators}),
+    userPassword: new FormControl<string>('', {validators: this._validators})
   })
 
   constructor(private fb:FormBuilder, public domains:DomainsService){
@@ -31,13 +35,14 @@ export class LoginFormComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.form.valueChanges.subscribe((vc) => console.log('FORM', vc))
+    this.form.valueChanges.subscribe((vc) => {
+      console.log('FORM', vc);
+    })
 
 
     this.domains.current$
     .pipe(takeUntil(this.destroy$))
     .subscribe((domain) => {
-      //TODO
       if(domain && (this.domainId.value === 0 || this.domainId.value !== domain.domainId)) {
         this.domainId.patchValue(domain.domainId);
       } else {
